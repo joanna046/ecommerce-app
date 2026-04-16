46bfbb6925664575c3bdadffc55766755ef87191
joanna046 <joannasaji046@gmail.com>
2026-04-16 13:50:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+// Route
 app.get('/', (req, res) => {
   res.send('Joanna Saji - Deployment Success 🚀');
 });
 
-app.listen(3000, () => {
+// IMPORTANT: bind to 0.0.0.0 for EC2/Docker
+app.listen(3000, '0.0.0.0', () => {
   console.log('Server running on port 3000');
 });
